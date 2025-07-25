1091bf7fd0b8c147b12363197057fe3281006f33
robslaw13958 <robslaw13958@gmail.com>
2025-07-25 17:31:29 +0200

    Design changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Cormorant_Garamond, Alegreya } from "next/font/google";
 import "./globals.css";
 import styles from "@/app/page.module.css";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const cormorant = Cormorant_Garamond({
+  variable: "--font-cormorant",
   subsets: ["latin"],
+  weight: "400",
+  style: ["normal", "italic"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const alegreya = Alegreya({
+  variable: "--font-alegreya",
   subsets: ["latin"],
+  weight: "400",
+  style: ["normal", "italic"],
 });
 
 export const metadata = {
@@ -23,7 +27,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${cormorant.variable} ${alegreya.variable}`}>
         <header>
           <h1>Agroturystyka Łysogóry</h1>
           <nav>
@@ -37,22 +41,24 @@ export default function RootLayout({ children }) {
           </nav>
         </header>
         {children}
-        <footer className={styles.footer}>
-          <div>
-            <iframe
+        <footer>
+          <div className={styles.footerContent}>
+            <div>
+              <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d302.8422210753319!2d20.80063107662038!3d50.91761022662226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471823de103a0a07%3A0x6862aea440e4360c!2sCiekoty%205E%2C%2026-001%20Ciekoty!5e1!3m2!1spl!2spl!4v1750889255012!5m2!1spl!2spl"
                 width="100%" height="200" allowFullScreen="" loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"></iframe>
-          </div>
-          <div>
-            <h3>Kontakt</h3>
-            <p>Tel. 889 446 126</p>
-            <p>Tel. 888 435 423</p>
-          </div>
-          <div>
-            <h3>Dane do płatności</h3>
-            <p>ING Bank Śląski</p>
-            <p>21 1050 1416 1000 0090 7237 3732</p>
+            </div>
+            <div>
+              <h3>Kontakt</h3>
+              <p>Tel. 889 446 126</p>
+              <p>Tel. 888 435 423</p>
+            </div>
+            <div>
+              <h3>Dane do płatności</h3>
+              <p>ING Bank Śląski</p>
+              <p>21 1050 1416 1000 0090 7237 3732</p>
+            </div>
           </div>
         </footer>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.page}>
-      <h2>Witamy w Agroturystyce Łysogóry!</h2>
+      <h2>Witamy w naszej agroturystyce!</h2>
       <p>Szukasz miejsca, gdzie czas płynie wolniej, a śpiew ptaków budzi Cię rano? Nasz dom położony tuż przy lesie (dosłownie 10 metrów!) to idealna przystań dla rodzin z dziećmi, grup przyjaciół i wszystkich spragnionych odpoczynku z dala od miejskiego zgiełku. Oferujemy komfortowe apartamenty, mnóstwo atrakcji i domową atmosferę, w której poczujesz się naprawdę swobodnie.</p>
       <h2>Co nas wyróżnia?</h2>
       <ul>
-        <li>🌳 Spokój i bliskość natury</li>
+        <li><a href="/outside/IMG-20250625-WA0059.jpg">🌳 Spokój i bliskość natury</a></li>
         <li>👨‍👩‍👧‍👦 Idealne miejsce dla rodzin</li>
         <li>🔥 Miejsce na ognisko</li>
         <li>🏡 Przeszklona weranda na każdą pogodę</li>
